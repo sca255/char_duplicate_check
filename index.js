d5c0de6217a61d9d03f246ef791c2fd3ca7aee59
arpan das <88543780+sca255@users.noreply.github.com>
2024-08-20 08:41:41 +0000

    v1test

--- index.js
+++ index.js
@@ -36,10 +36,9 @@ function onExampleInput(event) {
 var ctx=getContext()
 // This function is called when the button is clicked
 function onButtonClick() {
-  chars=ctx.characters;
+  let chars=ctx.characters;
   for (let i in chars){
-    let s=""
-    s.ends
+  
     if (i.name.endsWith("1")||i.name.endsWith("2")||i.name.endsWith("3")||i.name.endsWith("4")||i.name.endsWith("5")||i.name.endsWith("6")||i.name.endsWith("7")||i.name.endsWith("8")||i.name.endsWith("9")){
       deleteCharacter(i.avatar)
     }
