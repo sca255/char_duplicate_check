2ecad5ce18b30cd9be26339c9dc7cf1deeb9c2db
arpan das <88543780+sca255@users.noreply.github.com>
2024-08-20 08:54:18 +0000

    v1fix2

--- index.js
+++ index.js
@@ -38,9 +38,11 @@ var ctx=getContext()
 function onButtonClick() {
   let chars=ctx.characters;
   for (let i in chars){
+    let i2=ctx.characters[i]
     console.log(i)
-    if (i.name.endsWith("1")||i.name.endsWith("2")||i.name.endsWith("3")||i.name.endsWith("4")||i.name.endsWith("5")||i.name.endsWith("6")||i.name.endsWith("7")||i.name.endsWith("8")||i.name.endsWith("9")){
-      deleteCharacter(i.avatar)
+
+    if (i2.name.endsWith("1")||i.name.endsWith("2")||i.name.endsWith("3")||i.name.endsWith("4")||i.name.endsWith("5")||i.name.endsWith("6")||i.name.endsWith("7")||i.name.endsWith("8")||i.name.endsWith("9")){
+      deleteCharacter(i2.avatar)
     }
   }
 
